caa8e75b6ca5293a1a74f1fe452de5edf19f62ca
Débora Hellen <deboraveras.inf1.ep@outlook.com>
2025-09-22 18:57:33 -0300

    feat: criacao e teste do toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,33 @@
+import { useEffect } from 'react';
+import useToastLoading from './hooks/useToastLoading';
+import { ToastContainer } from 'react-toastify';
+
 export default function App() {
-  return <div className="font-black">Olá Mundo!!</div>;
+  const toastLoading = useToastLoading();
+
+  useEffect(() => {
+    toastLoading({
+      mensagem: 'Login realizado com sucesso',
+      tipo: 'success',
+    });
+  }, []);
+
+  return (
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        rtl={false}
+        draggable
+        pauseOnHover
+        closeButton={true}
+        style={{ width: 'fit-content' }}
+      />
+      <div className="bg-gray-100 h-screen">
+        <div className="font-black">Olá Mundo!!</div>
+      </div>
+    </>
+  );
 }
